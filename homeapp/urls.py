5cc107f472ac6e044bccf77c9cb8a3a152cb6bfe
saifulvisualindie <saiful@visualindie.com>
2023-11-16 21:32:01 +0800

    student pages created

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('login/', views.LoginView, name='login'),
     path('userprofile/', views.UserProfileView, name='userprofile'),
     path('logout/', views.LogoutView, name='logout'),
+    path('student/', views.StudentIndexView, name='student_index'),
+    path('student/textbook/', views.StudentTextbookView, name='student_textbook'),
+    path('student/payment/', views.StudentPaymentView, name='student_payment'),
+    path('library/', views.LibraryIndexView, name='library_index'),
+    path('finance/', views.FinanceIndexView, name='finance_index'),
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -2,20 +2,71 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserForm
-from .decorators import redirect_authenticated_user
+from .decorators import redirect_authenticated_user, admin_required, student_required, library_required, finance_required
 from django.contrib import messages
 import secrets
 
 
 @login_required(login_url='homeapp:login')
+@admin_required
 def IndexView(request):
     user = request.user
 
     context = {
-        'Hello': 'Hello',
+        'user': user,
     }
 
     return render(request, 'index.html', context)
+
+@login_required(login_url='homeapp:login')
+@student_required
+def StudentIndexView(request):
+    user = request.user
+
+    context = {
+        'user': user,
+    }
+    return render(request, 'student/student_index.html', context)
+
+@login_required(login_url='homeapp:login')
+@student_required
+def StudentTextbookView(request):
+    user = request.user
+
+    context = {
+        'user': user,
+    }
+    return render(request, 'student/student_textbook.html', context)
+
+@login_required(login_url='homeapp:login')
+@student_required
+def StudentPaymentView(request):
+    user = request.user
+
+    context = {
+        'user': user,
+    }
+    return render(request, 'student/student_payment.html', context)
+
+@login_required(login_url='homeapp:login')
+@library_required
+def LibraryIndexView(request):
+    user = request.user
+
+    context = {
+        'user': user,
+    }
+    return render(request, 'library/library_index.html', context)
+
+@login_required(login_url='homeapp:login')
+@finance_required
+def FinanceIndexView(request):
+    user = request.user
+
+    context = {
+        'user': user,
+    }
+    return render(request, 'finance/finance_index.html', context)
 
 
 @login_required(login_url='homeapp:login')
@@ -37,14 +88,34 @@ def UserProfileView(request):
 
     return render(request, 'userprofile.html', context)
 
-@redirect_authenticated_user
 def LoginView(request):
+    if request.user.is_authenticated:
+        if request.user.is_superuser:
+            return redirect('homeapp:index')
+        elif request.user.is_student:
+            return redirect('homeapp:student_index')
+        elif request.user.is_library:
+            return redirect('homeapp:library_index')
+        elif request.user.is_finance:
+            return redirect('homeapp:finance_index')
+        else:
+            return redirect('homeapp:index')
+
     if request.method == 'POST':
         loginform = LoginForm(data=request.POST)
         if loginform.is_valid():
             user = loginform.get_user()
             login(request, user)
-            return redirect('homeapp:index')
+            if user.is_superuser:
+                return redirect('homeapp:index')
+            elif user.is_student:
+                return redirect('homeapp:student_index')
+            elif user.is_library:
+                return redirect('homeapp:library_index')
+            elif user.is_finance:
+                return redirect('homeapp:finance_index')
+            else:
+                return redirect('homeapp:index')
     else:
         loginform = LoginForm()
 
