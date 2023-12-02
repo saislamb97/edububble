440e6bced586fe1cf637adf8d23cb7d54db4b12c
saiful <sa.islamb97@gmail.com>
2023-12-03 00:04:40 +0800

    application updated to only textbook system

--- homeapp/models.py
+++ homeapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
-import os
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.core.mail import send_mail
-from django.db.models.signals import post_save, m2m_changed
+from django.db.models.signals import post_save, m2m_changed, pre_save
 from django.dispatch import receiver
 import random
 
@@ -48,8 +48,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     fullname = models.CharField(max_length=250, blank=True)
     start_date = models.DateTimeField(default=timezone.now)
     is_student = models.BooleanField(default=False)
-    is_library = models.BooleanField(default=False)
-    is_finance = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
@@ -63,7 +62,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     def clean(self):
         # Check if the user has multiple roles selected
-        roles_count = sum([self.is_student, self.is_library, self.is_finance])
+        roles_count = sum([self.is_student, self.is_teacher])
 
         if roles_count > 1:
             raise ValidationError("A user cannot have multiple roles simultaneously.")
@@ -106,8 +105,6 @@ class Students(models.Model):
         ('sec_e', 'Section E'),
     ]
     section = models.CharField(max_length=250, blank=True, null=True, choices=SECTION_CHOICES)
-    total_credit = models.IntegerField(blank=True, null=True)
-    total_due = models.IntegerField(blank=True, null=True)
     textbooks = models.ManyToManyField(Textbooks, blank=True)
 
     def __str__(self):
@@ -136,7 +133,7 @@ def delete_student_profile(sender, instance, **kwargs):
         # If the user is not marked as a student, delete the associated Students instance
         Students.objects.filter(username=instance).delete()
     
-class StudentTextbook(models.Model):
+class TextbookStatus(models.Model):
     student = models.ForeignKey(Students, on_delete=models.CASCADE)
     textbook = models.ForeignKey(Textbooks, on_delete=models.CASCADE)
     STATUS_CHOICES = [
@@ -145,6 +142,7 @@ class StudentTextbook(models.Model):
         ('returned', 'Returned'),
     ]
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    previous_status = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return f"{self.student.username} - {self.textbook.book_title} - {self.status}"
@@ -154,69 +152,37 @@ def create_student_textbook(sender, instance, action, **kwargs):
     if action == 'post_add':
         textbooks = kwargs['pk_set']
         for textbook_id in textbooks:
-            textbook = Textbooks.objects.get(id=textbook_id)
-            StudentTextbook.objects.create(student=instance, textbook=textbook, status='not_collected')
+            textbook = get_object_or_404(Textbooks, id=textbook_id)
+            TextbookStatus.objects.create(student=instance, textbook=textbook, status='not_collected')
 
-def payslip_file_extension(value):
-    ext = os.path.splitext(value.name)[1]  # Get the file extension
-    valid_extensions = ['.jpg', '.jpeg', '.png', '.pdf']
-    if not ext.lower() in valid_extensions:
-        raise ValidationError('Unsupported file extension. Supported extensions are .jpg, .jpeg, .png, .pdf')
+@receiver(post_save, sender=TextbookStatus)
+def textbook_status_changed(sender, instance, **kwargs):
+    if not kwargs.get('created'):  # Send notifications on updates
+        user_email = instance.student.username.email
+        status = instance.status
+        old_status = instance.previous_status  # Retrieve the previous status from the model field
+        if status != old_status:
+            if status == 'collected':
+                send_collect_notification(user_email)
+            elif status == 'returned':
+                send_return_notification(user_email)
 
-class PaymentApplication(models.Model):
-    student = models.ForeignKey(Students, on_delete=models.CASCADE)
-    application_id = models.CharField(max_length=250, default=generate_unique_id, unique=True)
-    application_date = models.DateTimeField(default=timezone.now)
-    paying_amount = models.IntegerField()
-    payslip = models.FileField(upload_to='payslips/', validators=[payslip_file_extension])
-    is_pending = models.BooleanField(default=True)
-    is_approved = models.BooleanField(default=False)
-    is_rejected = models.BooleanField(default=False)
+@receiver(pre_save, sender=TextbookStatus)
+def track_previous_status(sender, instance, **kwargs):
+    if instance.pk:  # Check if the instance already exists (update operation)
+        try:
+            old_instance = sender.objects.get(pk=instance.pk)
+            instance.previous_status = old_instance.status  # Track the previous status before saving
+        except sender.DoesNotExist:
+            print("The instance does not exist yet.")
 
-    def __str__(self):
-        return str(self.student)
-    
-    def clean(self):
-        # Check if the application has multiple statuses selected
-        statuses_count = sum([self.is_pending, self.is_approved, self.is_rejected])
 
-        if statuses_count > 1:
-            raise ValidationError("A payment application cannot have multiple statuses simultaneously.")
-
-    def save(self, *args, **kwargs):
-        
-        # Check if the application is approved
-        if self.is_approved:
-            # Update the related Students model
-            self.student.total_due -= self.paying_amount
-            self.student.total_credit += self.paying_amount
-            self.student.save()
-
-        super(PaymentApplication, self).save(*args, **kwargs)
-
-@receiver(post_save, sender=PaymentApplication)
-def payment_application_changed(sender, instance, **kwargs):
-    # Check if the approval status changed
-    user_email = instance.student.username.email 
-    if getattr(instance, '_original_is_approved', False) != instance.is_approved:
-        send_approval_notification(user_email)
-    elif getattr(instance, '_original_is_rejected', False) != instance.is_rejected:
-        send_rejection_notification(user_email)
-
-# Save the original values when the instance is loaded
-@receiver(post_save, sender=PaymentApplication)
-def save_original_values(sender, instance, **kwargs):
-    instance._original_is_approved = instance.is_approved
-    instance._original_is_rejected = instance.is_rejected
-
-def send_approval_notification(email):
-    # Implement your email sending logic here for approval notification
-    subject = 'Payment Application Approved'
-    message = 'Your payment application has been approved.'
+def send_collect_notification(email):
+    subject = 'Textbook Status Changed'
+    message = 'You have collected your book.'
     send_mail(subject, message, 'sa.islamb97@gmail.com', [email])
 
-def send_rejection_notification(email):
-    # Implement your email sending logic here for rejection notification
-    subject = 'Payment Application Rejected'
-    message = 'Your payment application has been rejected.'
+def send_return_notification(email):
+    subject = 'Textbook Status Changed'
+    message = 'You have returned your book.'
     send_mail(subject, message, 'sa.islamb97@gmail.com', [email])
--- homeapp/views.py
+++ homeapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import login,logout
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, PaymentApplicationForm, UserForm
-from .decorators import admin_required, student_required, library_required, finance_required
+from .forms import LoginForm, UserForm
+from .decorators import admin_required, student_required, teacher_required
 from django.contrib import messages
-from .models import ClassName, StudentTextbook, Students, Textbooks, PaymentApplication
+from .models import ClassName, TextbookStatus, Students, Textbooks
 
 
 # User Index Views
@@ -24,39 +24,6 @@ def IndexView(request):
 def StudentIndexView(request):
     user = request.user
 
-    context = {
-        'user': user,
-    }
-
-    return render(request, 'student/student_index.html', context)
-
-@login_required(login_url='homeapp:login')
-@library_required
-def LibraryIndexView(request):
-    user = request.user
-
-    context = {
-        'user': user,
-    }
-    return render(request, 'library/library_index.html', context)
-
-@login_required(login_url='homeapp:login')
-@finance_required
-def FinanceIndexView(request):
-    user = request.user
-
-    context = {
-        'user': user,
-    }
-    return render(request, 'finance/finance_index.html', context)
-
-
-# User Other Views
-@login_required(login_url='homeapp:login')
-@student_required
-def StudentTextbookView(request):
-    user = request.user
-
     if user.is_authenticated and user.is_student:
         student_profile = Students.objects.get(username=user)
         user_textbooks = student_profile.textbooks.all()
@@ -66,7 +33,7 @@ def StudentTextbookView(request):
         for textbook in user_textbooks:
             class_name = ClassName.objects.get(pk=textbook.classname_id)
             # Retrieve the corresponding StudentTextbook instance
-            student_textbook = StudentTextbook.objects.filter(student=student_profile, textbook=textbook).first()
+            student_textbook = TextbookStatus.objects.filter(student=student_profile, textbook=textbook).first()
             textbooks_with_status.append({
                 'textbook': textbook,
                 'class_name': class_name.classname,
@@ -74,53 +41,17 @@ def StudentTextbookView(request):
             })
         
         context = {
-            'textbooks_with_status': textbooks_with_status
+            'textbooks_with_status': textbooks_with_status,
+            'user': user,
         }
 
-        return render(request, 'student/student_textbook.html', context)
+
+    return render(request, 'student_index.html', context)
 
 @login_required(login_url='homeapp:login')
-@student_required
-def StudentPaymentView(request):
+@teacher_required
+def teacherIndexView(request):
     user = request.user
-
-    if user.is_authenticated and user.is_student:
-        student_profile = Students.objects.get(username=user)
-        user_payments = PaymentApplication.objects.filter(student=student_profile)
-        
-        context = {
-            'user_payments': user_payments
-        }
-
-    return render(request, 'student/student_payment.html', context)
-
-@login_required(login_url='homeapp:login')
-@student_required
-def StudentPaymentApplicationView(request):
-    user = request.user
-
-    if user.is_authenticated and user.is_student:
-        student_profile = Students.objects.get(username=user)
-        
-        if request.method == 'POST':
-            paymentform = PaymentApplicationForm(request.POST, request.FILES)
-            if paymentform.is_valid():
-                payment_application = paymentform.save(commit=False)
-                payment_application.student = student_profile
-                payment_application.save()
-                return redirect('homeapp:student_payment')
-        else:
-            paymentform = PaymentApplicationForm()
-
-        context = {
-            'paymentform': paymentform
-        }
-        
-    return render(request, 'student/student_payment_application.html', context)
-
-@login_required(login_url='homeapp:login')
-@library_required
-def LibraryTextbookView(request):
     if request.method == 'POST':
         student_id = request.POST.get('student_id')
         textbook_id = request.POST.get('textbook_id')
@@ -128,7 +59,7 @@ def LibraryTextbookView(request):
 
         student = get_object_or_404(Students, id=student_id)
         textbook = get_object_or_404(Textbooks, id=textbook_id)
-        student_textbook = get_object_or_404(StudentTextbook, student=student, textbook=textbook)
+        student_textbook = get_object_or_404(TextbookStatus, student=student, textbook=textbook)
 
         # Update the status
         student_textbook.status = status
@@ -136,7 +67,7 @@ def LibraryTextbookView(request):
         messages.success(request, 'Textbook status updated.')
 
         # Redirect or handle the response as needed
-        return redirect('homeapp:library_textbook')
+        return redirect('homeapp:teacher_index')
 
     classes = ClassName.objects.all()
     class_students_textbooks = []
@@ -150,7 +81,7 @@ def LibraryTextbookView(request):
             textbooks_with_status = []
             
             for textbook in textbooks:
-                student_textbook = StudentTextbook.objects.filter(student=student, textbook=textbook).first()
+                student_textbook = TextbookStatus.objects.filter(student=student, textbook=textbook).first()
                 textbooks_with_status.append({
                     'textbook': textbook,
                     'status': student_textbook.status if student_textbook else 'Not Specified'
@@ -167,61 +98,11 @@ def LibraryTextbookView(request):
         })
 
     context = {
-        'class_students_textbooks': class_students_textbooks
+        'class_students_textbooks': class_students_textbooks,
+        'user': user,
     }
-    
-    return render(request, 'library/library_textbook.html', context)
 
-@login_required(login_url='homeapp:login')
-@finance_required
-def FinancePaymentView(request):
-    classes = ClassName.objects.all()
-    class_students_payments = []
-
-    for class_obj in classes:
-        students = Students.objects.filter(classname=class_obj)
-        class_students = []
-
-        for student in students:
-            payments = PaymentApplication.objects.filter(student=student)
-            class_students.append({
-                'student': student,
-                'payments': payments
-            })
-
-        class_students_payments.append({
-            'class_obj': class_obj,
-            'class_students': class_students
-        })
-
-    if request.method == 'POST':
-        payment_id = request.POST.get('payment_id')
-        payment = get_object_or_404(PaymentApplication, id=payment_id)
-        
-        is_approved = request.POST.get('is_approved')
-        is_rejected = request.POST.get('is_rejected')
-
-        if is_approved:
-            payment.is_pending = False
-            payment.is_approved = True
-            payment.is_rejected = False
-        elif is_rejected:
-            payment.is_pending = False
-            payment.is_approved = False
-            payment.is_rejected = True
-
-        payment.save()
-        messages.success(request, 'Payment status updated.')
-
-        return redirect('homeapp:finance_payment')
-
-    context = {
-        'class_students_payments': class_students_payments
-    }
-    
-    return render(request, 'finance/finance_payment.html', context)
-
-
+    return render(request, 'teacher_index.html', context)
 
 # User Profile and Login Views
 @login_required(login_url='homeapp:login')
@@ -249,10 +130,8 @@ def LoginView(request):
             return redirect('homeapp:index')
         elif request.user.is_student:
             return redirect('homeapp:student_index')
-        elif request.user.is_library:
-            return redirect('homeapp:library_index')
-        elif request.user.is_finance:
-            return redirect('homeapp:finance_index')
+        elif request.user.is_teacher:
+            return redirect('homeapp:teacher_index')
         else:
             return redirect('homeapp:index')
 
@@ -265,10 +144,8 @@ def LoginView(request):
                 return redirect('homeapp:index')
             elif user.is_student:
                 return redirect('homeapp:student_index')
-            elif user.is_library:
-                return redirect('homeapp:library_index')
-            elif user.is_finance:
-                return redirect('homeapp:finance_index')
+            elif user.is_teacher:
+                return redirect('homeapp:teacher_index')
             else:
                 return redirect('homeapp:index')
     else:
